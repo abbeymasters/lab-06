8795825afb4897d7f0aa30f340a4a322a360a8cb
Abbey Masters <abbeykmasters@gmail.com>
2019-09-23 16:43:41 -0700

    all routes working

---
+++ run-model.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const connect = require('./lib/connect');
+const mongoose = require('mongoose');
+
+connect();
+
+const Book = require('./lib/models/book');
+
+Book.create({
+    name: 'americanah',
+    author: 'chimamanda ngozi adichie',
+    available: true,
+    yearPublished: 2013,
+    howMany: 5,
+    genre: ['fiction'],
+    appearances: {
+      pattern: 'solid',
+      mainColor: 'white'
+    }})
+    .then(createdBook => {
+      console.log(createdBook);
+    })
+    .then(() => {
+      mongoose.disconnect();
+});
+
+
--- server.js
+++ server.js
@@ -15,4 +15,40 @@ app.get('/api/books', (req, res, next) => {
     .catch(next);
 });
 
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findById(req.params.id)
+    .then(book => {
+      res.json(book);
+    })
+    .catch(next);
+});
+
+app.post('/api/books', (req, res, next) => {
+  Book.create(req.body)
+    .then(book => {
+      res.json(book);
+    })
+    .catch(next);
+});
+
+app.put('/api/books/:id', (req, res, next) => {
+  Book.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
+    .then(book => {
+      res.json(book);
+    })
+    .catch(next);
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.findByIdAndRemove(req.params.id)
+    .then(removed => {
+      res.json(removed);
+    })
+    .catch(next);
+});
+
 app.listen(3000, () => console.log('server running on 3000'));
