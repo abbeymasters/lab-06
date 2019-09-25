2c3ccb6e5458ace25f7fcb49f567ad34653aeb98
Abbey Masters <abbeykmasters@gmail.com>
2019-09-25 15:25:25 -0700

    finished app.test.js

--- e2e/_tests_/app.test.js
+++ e2e/_tests_/app.test.js
@@ -1,29 +1,31 @@
 const request = require('../request');
 
-describe('core app api', () => {
-  it('is alive', () => {
+describe('book app api', () => {
+  
+  it('checking if working', () => {
     return request
       .get('/hello')
-      .expect(200)
+      expect(200)
       .then(res => {
         expect(res.text).toBe('hello express');
       });
   });
 
-  it('returns 404 on non-api bad path', () => {
+  it('returns a 404 if on a bad path', () => {
     return request
-      .get('/bad-path')
+      .get('/1234')
       .expect(404)
       .expect('Content-Type', /text/);
   });
 
-  it('returns application/json 404 on bad api path', () => {
+  it('returns application/json 404 on a bad path', () => {
     return request
-      .post('/api/bad-path')
+      .post('/api/1234')
       .expect(404)
       .expect('Content-Type', /json/)
       .then(res => {
         expect(res.body.error).toMatch(/not found/i);
       });
   });
+
 });
